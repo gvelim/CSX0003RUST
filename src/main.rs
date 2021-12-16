9b28efec2f0a86cd6c04d476de62591c63fba57c
gvelim <github@velimachitis.me>
2021-12-16 13:27:08 +0000

    final

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,11 @@ fn main() {
 
     println!("array: {:?}", merge_sort(&mut v));
 }
-
+/// Sort function based on the merge sort algorithm
 fn merge_sort(v:&mut Vec<i32>) -> Vec<i32> {
 
+    /// Merge subroutine
+    /// Join to slices while in the right Order
     fn merge(left:&Vec<i32>, right:&Vec<i32>) -> Vec<i32> {
 
         enum Condition {
@@ -23,7 +25,10 @@ fn merge_sort(v:&mut Vec<i32>) -> Vec<i32> {
         let mut r = 0;
         let mut l = 0;
 
-        let cond = loop {
+        // go into a loop until one of the slices goes off bounds
+        // indicate which slice caused the exit for use by the
+        // following append instruction
+        let exit_cond = loop {
             if right[r] > left[l] {
                 //println!("({},{}) push: {}",rv,lv,lv);
                 output.push(left[l]);
@@ -41,11 +46,11 @@ fn merge_sort(v:&mut Vec<i32>) -> Vec<i32> {
             }
         };
 
-        match cond {
+        // append the remaining slice on the output vector
+        match exit_cond {
             Condition::LeftExit => output.extend_from_slice(&right[r..]),
             Condition::RightExit => output.extend_from_slice(&left[l..]),
         }
-
 
         print!("output: {},{:?} <> {},{:?},",rlen,right,llen,left);
         println!("=> {:?},",output);
@@ -55,18 +60,22 @@ fn merge_sort(v:&mut Vec<i32>) -> Vec<i32> {
     let len = v.len();
     println!("Input: ({}){:?} =>", len, v);
     match len {
+        // unity slice, just return it
         1 => v.clone(),
+        // sort the binary slice and exit
         2 => {
             if v[0] > v[1] {
                 v.swap(0, 1);
             }
             v.clone()
         },
+        // if slice length longer than 2 then split recursively
         _ => {
             let (left, right) = v.split_at(len >> 1 );
             let l = merge_sort(&mut Vec::from(left) );
             let r =  merge_sort(&mut Vec::from(right) );
 
+            // return a vector of the merged but ordered slices
             merge(&l,&r)
         }
     }

816e66d3715ca6559fea735d4aa69d54b2760014
gvelim <github@velimachitis.me>
2021-12-24 11:15:36 +0000

    added - pop_first() - push_first()
    refactored
    - pop -> pop_last()
    - push -> push_last()

--- src/linkedlists.rs
+++ src/linkedlists.rs
@@ -16,8 +16,8 @@ impl<T> List<T>
     pub fn new() -> List<T> {
         List::Empty
     }
-    /// Push to the end of the list. inefficiently for now
-    pub fn push(&mut self, item: T) {
+    /// Push to the end of the list; popped first
+    pub fn push_last(&mut self, item: T) {
         match self {
             List::Empty => {
                 *self = List::NotEmpty(
@@ -26,26 +26,65 @@ impl<T> List<T>
                 );
             }
             List::NotEmpty(_, next) => {
-                next.push(item);
+                next.push_last(item);
             }
         }
     }
-    /// Pop from the end of the list, inefficiently for now
-    pub fn pop(&mut self) -> Option<T> {
+    /// Push to the head of the list; popped last
+    pub fn push_first(&mut self, item: T) {
+        if let List::Empty = self {
+            self.push_last(item);
+        } else {
+            // create a memory space to hold current head
+            let mut old_head = List::Empty;
+            // move current head to new memory space
+            // while we have self ready to hold the new head
+            std::mem::swap(self, &mut old_head);
+            // create new head that points to old one
+            *self = List::NotEmpty(
+                item,
+                Box::new(old_head),
+            );
+        }
+    }
+    /// Pop from the end of the list
+    pub fn pop_last(&mut self) -> Option<T> {
         match self {
             List::Empty => None,
             List::NotEmpty(val, next) => {
                 if List::Empty == **next {
+                    // we arrived at the last one, since next == null
+                    // move value out
                     let item = *val;
+                    // make box from previous node to hold empty
                     *self = List::Empty;
                     Some(item)
                 }
                 else {
-                    next.pop()
+                    // not the last, move to the next
+                    next.pop_last()
                 }
             }
         }
     }
+    /// Pop for the head of the list
+    pub fn pop_first(&mut self) -> Option<T> {
+        match self {
+            List::Empty => None,
+            List::NotEmpty(item, next) => {
+                // move ownership
+                let val = *item;
+                // create a new empty head
+                let mut new_head = Box::new(List::Empty);
+                // move next node into the new head
+                std::mem::swap( next, &mut new_head);
+                // self take ownership of new head
+                *self = *new_head;
+                Some(val)
+            }
+        }
+    }
+    /// Iterate by
     pub fn iter(&self) -> ListIterByRef<T> {
         match self {
             List::Empty =>
@@ -64,7 +103,7 @@ impl<T> Iterator for List<T>
     where T: Copy + Clone + PartialEq {
     type Item = T;
     fn next(&mut self) -> Option<Self::Item> {
-        self.pop()
+        self.pop_first()
     }
 }
 
@@ -78,7 +117,7 @@ impl<T> FromIterator<T> for List<T>
     fn from_iter<I: IntoIterator<Item=T>>(iter: I) -> Self {
         let mut list = List::Empty;
         iter.into_iter()
-            .for_each( |item| list.push(item));
+            .for_each( |item| list.push_first(item));
         list
     }
 }
@@ -112,9 +151,27 @@ mod tests {
     fn test_push() {
         let mut list = List::new();
 
-        list.push(1);
-        list.push(2);
-        list.push(3);
+        list.push_first(1);
+        list.push_first(2);
+        list.push_first(3);
+
+        assert_eq!(list,
+                   List::NotEmpty(3,Box::new(
+                       List::NotEmpty(2, Box::new(
+                           List::NotEmpty(1, Box::new(
+                               List::Empty
+                           ))
+                       ))
+                   ))
+        )
+    }
+    #[test]
+    fn test_push_last() {
+        let mut list = List::new();
+
+        list.push_last(1);
+        list.push_last(2);
+        list.push_last(3);
 
         assert_eq!(list,
                    List::NotEmpty(1,Box::new(
@@ -129,19 +186,30 @@ mod tests {
     #[test]
     fn test_pop() {
         let mut l = List::new();
-        l.push(1);
-        l.push(2);
+        l.push_last(1);
+        l.push_last(2);
 
-        assert_eq!(l.pop(), Some(2));
-        assert_eq!(l.pop(), Some(1));
-        assert_eq!(l.pop(), None);
-        assert_eq!(l.pop(), None);
+        assert_eq!(l.pop_first(), Some(1));
+        assert_eq!(l.pop_first(), Some(2));
+        assert_eq!(l.pop_first(), None);
+        assert_eq!(l.pop_first(), None);
+    }
+    #[test]
+    fn test_pop_last() {
+        let mut l = List::new();
+        l.push_last(1);
+        l.push_last(2);
+
+        assert_eq!(l.pop_last(), Some(2));
+        assert_eq!(l.pop_last(), Some(1));
+        assert_eq!(l.pop_last(), None);
+        assert_eq!(l.pop_last(), None);
     }
     #[test]
     fn test_iter() {
         let mut l = List::new();
-        l.push(1);
-        l.push(2);
+        l.push_last(1);
+        l.push_last(2);
 
         let mut iter = l.iter();
 
@@ -155,8 +223,8 @@ mod tests {
     #[test]
     fn test_into_iter() {
         let mut l = List::new();
-        l.push(1);
-        l.push(2);
+        l.push_last(1);
+        l.push_last(2);
 
         let mut iter = l.into_iter();
 
@@ -173,9 +241,9 @@ mod tests {
 
         let mut l : List<i32> = v.into_iter().collect();
 
-        assert_eq!(l.pop(), Some(3));
-        assert_eq!(l.pop(), Some(2));
-        assert_eq!(l.pop(), Some(1));
-        assert_eq!(l.pop(), None);
+        assert_eq!(l.pop_last(), Some(3));
+        assert_eq!(l.pop_last(), Some(2));
+        assert_eq!(l.pop_last(), Some(1));
+        assert_eq!(l.pop_last(), None);
     }
 }

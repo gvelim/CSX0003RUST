0fef7eaf9bd9a39894236b187796e7fee2826a60
gvelim <github@velimachitis.me>
2021-12-24 11:59:05 +0000

    Main - Create FIFO out of random numbers - From FIFO -> Vec, bTree - show results

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,18 +1,18 @@
-use mergeshort::dnc::merge_sort;
+use std::iter::from_fn;
+use mergeshort::dnc::*;
 use mergeshort::trees::*;
 use mergeshort::linkedlists::*;
 
 fn main() {
 
-    let v = vec![91,82,73,64,5,09,18,2,73,6,45,90,18,27,364,50,91,82,7,364,5];
-    let list : List<i32> = v.iter().map(|x| *x ).collect();
-    let bt : BinaryTree<i32> = list.iter().map(|x| *x).collect();
+    let list: List<i32> = from_fn(|| { Some(rand::random::<i8>() as i32) })
+        .take(25)
+        .collect();
+    let v: Vec<i32> = list.iter().map(|x| *x).collect();
+    let bt : BinaryTree<i32> = list.iter().map(|x| *x as i32).collect();
 
     println!("Merge Sort: {:?}", merge_sort(&v));
     println!("bTree Sort: {:?}", bt.iter().collect::<Vec<&i32>>());
-    println!("List      : {:?}", list.iter().collect::<Vec<&i32>>() );
+    println!("List      : {:?}", list.iter().collect::<Vec<_>>() );
 
-    for i in list {
-        print!("{},",i)
-    }
 }

5fb8e1ee4ef061d7943c76740dd253034f5bd91f
gvelim <github@velimachitis.me>
2022-01-10 11:37:03 +0000

    main <n> n: length of array to be sorted

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,13 +2,22 @@ use std::iter::from_fn;
 use csx3::sort::*;
 use csx3::trees::*;
 use csx3::linkedlists::*;
+use std::env::args;
+use std::str::FromStr;
 
 type MyType = i8;
 
 fn main() {
 
+    let n = args().into_iter()
+        .skip(1)
+        .map(|x| usize::from_str(&x) )
+        .next()
+        .expect("Use: main <n>\nn : number of elements to be sorted")
+        .unwrap_or(16);
+
     let list: List<MyType> = from_fn(|| { Some(rand::random::<MyType>()) })
-        .take(16)
+        .take(n)
         .collect();
     let mut v: Vec<MyType> = list.iter().map(|x| *x).collect();
     let bt : BinaryTree<MyType> = list.iter().map(|x| *x).collect();

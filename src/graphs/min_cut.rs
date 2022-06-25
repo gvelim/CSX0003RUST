d88246d498f00ff3e2d76b006e0ee7a7a3fe165c
george velimachitis <3185416+gvelim@users.noreply.github.com>
2022-06-25 12:44:37 +0100

    algo completes but destroys duplicate edges; storing so I can take an Edge based approach, rather a node one

--- src/graphs/min_cut.rs
+++ src/graphs/min_cut.rs
@@ -1,68 +1,65 @@
-use rand::Rng;
+use rand::{Rng, thread_rng};
 use super::*;
 
 trait MinimumCut {
-    fn min_cuts(&self) -> Option<HashSet<(Node,Node)>>;
+    fn min_cuts(&self) -> Option<Graph>;
 }
 
 impl MinimumCut for Graph {
-    fn min_cuts(&self) -> Option<HashSet<(Node,Node)>> {
+    fn min_cuts(&self) -> Option<Graph> {
 
         if self.edges.is_empty() {
             return None;
         }
-
-        let mut nodes = self.nodes.clone();
-        let mut edges = self.edges.clone().into_iter()
-            .fold( HashSet::<(Node,Node)>::new(),|mut edges, (src, dst)| {
-                dst.into_iter().for_each(|dst| {
-                   edges.insert((src,dst));
-                });
-                edges
-            });
-
-        println!("{:?}", edges);
+        let Graph { mut edges, mut nodes } = self.clone();
 
         while nodes.len() > 2 {
-            //println!("{:?}",edges);
-            // choose a random Node / Edge to start with;
-            let idx = rand::thread_rng().gen_range(1..edges.len());
-            let col_edge = edges.iter().nth(idx).copied().unwrap();
+            // pick at random src node out of the nodes remaining
+            let s_node = thread_rng().gen_range(1..edges.len()) as Node;
 
-            // extract edges from src node
-            // and remove dst node
-            edges.remove(&col_edge);
-            let col_src = col_edge.0;
-            let col_dst = col_edge.1;
-            nodes.remove(&col_dst);
+            // pick dst node out of the first edge in the adjacent list
+            // edge (src, dst)
+            let d_node = edges.get(&s_node).unwrap().clone().iter().nth(0).copied().unwrap();
+            println!("Edge({s_node},{d_node})");
 
-            println!("Edges: {:?} => {:?}", col_edge, edges);
-            println!("Nodes: {:?}", nodes);
-            // Collect all edges originating from destination node
+            // collect all incoming edges to src node
+            // remove src node from the adjacent list
+            let s_edges = edges.remove(&s_node).unwrap();
+            // collect all incoming edges ro dst node
+            // remove dst node from the adjacent list
+            let d_edges = edges.remove(&d_node).unwrap();
+            
+            // Collapsing src and dst into a new node
+            // the new node owns the collection of edges 
+            let mut col_edges: HashSet<Node> = s_edges.union(&d_edges).copied().collect();
 
-            let (del_edges, new_edges) = edges.iter()
-                .filter(|edge|
-                    col_dst == edge.1 || col_dst == edge.0
-                )
-                .fold( (HashSet::new(), HashSet::new()), |(mut old,  mut new), edge| {
-                    old.insert(*edge);
-                    if edge.1 == col_dst {
-                        new.insert( (edge.0, col_src));
-                    } else if edge.0 == col_dst {
-                        new.insert( (col_src, edge.1));
-                    } else {
-                        panic!("{:?}",edge);
-                    }
-                    new.remove(&(col_src, col_src));
-                    (old, new)
+            // clear edge collection from loops
+            col_edges.remove(&s_node);
+            col_edges.remove(&d_node);
+
+            print!("src: {:?}->{:?} ", s_node, s_edges);
+            print!("dst: {:?}->{:?} ", d_node, d_edges);
+            println!("col->{:?}",col_edges);
+
+            // scan the remaining graph and remove edges with reference to src and dst nodes
+            // for each node, remove edges refer to dst and src nodes
+            // since we don't create a new node out of src and dst
+            // we keep src as the new and remove dst only
+            edges.iter_mut()
+                .for_each(| (_, edges)| {
+                    edges.remove(&d_node);
                 });
-            println!("tmp: {:?} => -{:?} +{:?}", col_edge, del_edges, new_edges);
 
-            // find and replace all dst references to src node
-            del_edges.into_iter().for_each(|edge| { edges.remove(&edge); } );
-            new_edges.into_iter().for_each(|edge| { edges.insert( edge); } );
+            // remove dst node; already removed from adjacent list
+            nodes.remove(&d_node);
+            
+            // add src back, however as the new node with merged edges 
+            edges.entry(s_node).or_insert(col_edges);
+            println!("Round Finish: \nEdges:{:?}\nNodes:{:?}\n",edges, nodes);
+
         }
-        Some( edges )
+
+        Some( Graph{edges, nodes} )
     }
 }
 
@@ -104,7 +101,7 @@ mod test {
             vec![4, 1, 3, 2]
         ];
 
-        let g = Graph::import_edges( &adj_list).expect("Error: Couldn't load edges");
+        let g = Graph::import_edges( &adj_list ).expect("Error: Couldn't load edges");
         println!("{:?}",g.min_cuts());
     }
 }

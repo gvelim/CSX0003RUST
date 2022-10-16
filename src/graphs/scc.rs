50491ba9b085c0ba862c0c3329c1031120fbed1b
george velimachitis <3185416+gvelim@users.noreply.github.com>
2022-10-16 11:32:28 +0100

    fn scc() v1 completed however not all tests pass

--- src/graphs/scc.rs
+++ src/graphs/scc.rs
@@ -9,7 +9,7 @@ use crate::graphs::{
 };
 
 trait ConnectedComponents {
-    fn strongly_connected_components(&self) -> Option<HashMap<Node, HashSet<Node>>>;
+    fn strongly_connected_components(&self) -> Option<Vec<Vec<Node>>>;
 }
 
 impl Graph {
@@ -26,15 +26,16 @@ impl Graph {
     }
 }
 impl ConnectedComponents for Graph {
-    fn strongly_connected_components(&self) -> Option<HashMap<Node, HashSet<Node>>> {
+    fn strongly_connected_components(&self) -> Option<Vec<Vec<Node>>> {
 
-        struct DFS { tracker: Tracker, queue: BinaryHeap<Step>, time: Cost }
+        struct DFS { tracker: Tracker, queue: BinaryHeap<Step>, time: Cost, path: Vec<Node> }
         impl DFS {
             fn new(g: &Graph) -> DFS {
                 DFS {
                     tracker: g.get_tracker(Undiscovered, 0, None),
                     queue: BinaryHeap::new(),
-                    time: 0
+                    time: 0,
+                    path: Vec::new()
                 }
             }
             fn dfs(&mut self, g: &Graph, start: Node) {
@@ -43,7 +44,7 @@ impl ConnectedComponents for Graph {
                 self.tracker[start].visited(Discovered).distance(self.time);
 
                 // processing the edges
-                println!("Enter: {start}:{:?}", self.tracker[start]);
+                // println!("Enter: {start}:{:?}", self.tracker[start]);
                 if let Some(edges) = g.edges.get(&start) {
                     for &dst in edges {
                         let d = dst.into();
@@ -57,7 +58,8 @@ impl ConnectedComponents for Graph {
                 self.time += 1;
                 self.tracker[start].visited(Processed).distance(self.time);
                 self.queue.push(Step(start, self.time));
-                println!("Exit: {start}:{:?}", self.tracker[start]);
+                self.path.push(start);
+                // println!("Exit: {start}:{:?}", self.tracker[start]);
             }
         }
 
@@ -77,16 +79,18 @@ impl ConnectedComponents for Graph {
         scc = DFS::new( &rev_g );
 
         println!("{:?}",v);
+        let mut out = vec![vec![]];
         v.into_iter()
             .for_each(|Step(node, _)| {
                 if !scc.tracker[node].is_discovered() {
-                    println!("Found >>");
+                    scc.path.clear();
                     scc.dfs( &rev_g,node );
+                    println!("path: {:?}",scc.path);
+                    out.push(scc.path.clone() );
                 }
             });
 
-
-        Some(HashMap::default())
+        Some(out)
     }
 }
 
@@ -97,10 +101,10 @@ mod test {
     #[test]
     fn test_scc_small() {
         let test_data = vec![
-            ("src/graphs/txt/scc_simple.txt", &[3,2,1,1,0]),
-            ("src/graphs/txt/scc_input_mostlyCycles_1_8.txt", &[4,2,2,0,0]),
-            ("src/graphs/txt/scc_input_mostlyCycles_8_16.txt", &[13,1,1,1,0]),
-            ("src/graphs/txt/scc_input_mostlyCycles_12_32.txt", &[29,3,0,0,0])
+            ("src/graphs/txt/scc_simple.txt", vec![3,2,1,1,0]),
+            ("src/graphs/txt/scc_input_mostlyCycles_1_8.txt", vec![4,2,2,0,0]),
+            ("src/graphs/txt/scc_input_mostlyCycles_8_16.txt", vec![13,1,1,1,0]),
+            ("src/graphs/txt/scc_input_mostlyCycles_12_32.txt", vec![29,3,0,0,0])
         ];
 
         test_data.into_iter()
@@ -109,13 +113,16 @@ mod test {
                 let mc = g.strongly_connected_components();
                 assert!(mc.is_some());
                 println!(">> Scc: {:?}",mc);
-                let scc =
+                let mut scc =
                     mc.unwrap()
                         .into_iter()
-                        .fold(vec![], |mut out, (_,set)| {
-                            out.push(set.len());
+                        .take(5)
+                        .enumerate()
+                        .fold(vec![0;5], |mut out, (idx, set)| {
+                            out[idx] = set.len();
                             out
                         });
+                scc.sort_by(|a, b| b.cmp(a));
                 assert_eq!( scc, cuts );
                 println!("--------------------");
             });

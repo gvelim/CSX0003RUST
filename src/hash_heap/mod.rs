9b5f2343834772207ecf0b6b1dbad60cbdf6ab61
george velimachitis <3185416+gvelim@users.noreply.github.com>
2022-11-23 08:27:40 +0000

    2SUM calc routines - loop optimisation, adding every non-matching item on the HashMap hence dealing with "same position" case

--- src/hash_heap/mod.rs
+++ src/hash_heap/mod.rs
@@ -2,12 +2,18 @@ use std::collections::{HashMap};
 
 /// Finds ALL pairs that have their sum equal to the target value
 pub fn two_sum_all(nums: Vec<i32>, target: i32) -> Vec<(i32,i32)> {
-    let map : HashMap<_,_> = nums.as_slice().iter().zip(0..).collect();
+    let mut map : HashMap<_,_> = HashMap::new();
     nums.iter()
         .map(|b| target-b)
         .zip(0..)
         .filter_map(|(a,n)|
-            map.get(&a).and_then(|&idx| if idx != n { Some((n,idx)) } else {None}  )
+            match map.get(&a)  {
+                Some(&i) => Some((i, n)),
+                None => {
+                    map.insert(target-a,n);
+                    None
+                }
+            }
         )
         .fold(vec![], |mut out, pair| {
             out.push(pair);
@@ -17,16 +23,22 @@ pub fn two_sum_all(nums: Vec<i32>, target: i32) -> Vec<(i32,i32)> {
 
 /// Returns ONLY the first pair that sums up to the target value
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    let map : HashMap<_,_> = nums.iter().zip(0..).collect();
+    let mut map : HashMap<i32,i32> = HashMap::new();
     let s = nums.iter()
         .map(|b| target-b)
         .zip(0..)
         .filter_map(|(a,n)|
-            map.get(&a).and_then(|&idx| if idx != n { Some((n,idx)) } else {None} )
+            match map.get(&a) {
+                Some(&i) => Some((n, i)),
+                None => {
+                    map.insert(target-a,n);
+                    None
+                }
+            }
         )
         .next()
         .unwrap_or_else(||panic!("No two sum results to {target}"));
-    vec![s.0, s.1]
+    vec![s.1, s.0]
 }
 
 #[cfg(test)]
@@ -36,9 +48,9 @@ mod test {
     #[test]
     fn test_2_sum() {
         let data = vec![
-            (vec![2,7,11,15],9,vec![0,1]),
-            (vec![3,2,4],6,vec![1,2]),
-            (vec![3,3],6,vec![0,1]),
+            (vec![2,7,11,15],9,vec![0,1])
+            ,(vec![3,2,4],6,vec![1,2])
+            ,(vec![3,3],6,vec![0,1])
         ];
 
         for (nums, target, res) in data {
@@ -49,8 +61,8 @@ mod test {
     #[test]
     fn test_2_sum_all() {
         let data = vec![
-            (vec![1,6,2,8,7,11,15], 9, vec![(0,3), (2,4), (3,0), (4,2)]),
-            (vec![3,2,4], 6, vec![(1,2),(2,1)]),
+            (vec![1,6,2,8,7,11,15], 9, vec![(0,3), (2,4)]),
+            (vec![3,2,4], 6, vec![(1,2)]),
             (vec![3,3], 6, vec![(0,1)]),
         ];
 

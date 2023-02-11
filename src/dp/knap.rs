a929ce10ad353b67e26753396c580e07d048e120
george velimachitis <3185416+gvelim@users.noreply.github.com>
2023-02-11 15:13:53 +0000

    DP Knapsack - Pass - test_knapsack()

--- src/dp/knap.rs
+++ src/dp/knap.rs
@@ -30,16 +30,39 @@ fn parse(input: &str) -> Result<(usize,Vec<Object>),ParseIntError> {
 
 #[cfg(test)]
 mod test {
+    use std::cmp::max;
     use super::*;
+
+    fn print_knapsack(ks: &[Vec<usize>]) {
+        for (i,item) in ks.iter().enumerate() {
+            println!("{i}th :: {:?}", item);
+        }
+    }
 
     #[test]
     fn test_knapsack() {
-        let inp = std::fs::read_to_string("src/dp/txt/input_random_1_4_4.txt").unwrap_or_else(|e| panic!("{}",e));
-        let (knapsack, list) = parse(inp.as_str()).unwrap_or_else(|e| panic!("{e}"));
+        let inp = std::fs::read_to_string("src/dp/txt/input_random_5_10_10.txt").unwrap_or_else(|e| panic!("{}",e));
+        let (capacity, list) = parse(inp.as_str()).unwrap_or_else(|e| panic!("{e}"));
 
-        let dp = vec![0;list.len()+1];
+        println!("{:?}",(capacity, &list));
 
+        let mut dp = vec![ vec![ 0; capacity+1 ]; list.len()+1 ];
 
+        (1..dp.len())
+            .for_each(|i| {
+                for w in 1..=capacity {
+                    let item = &list[i-1];
+                    dp[i][w] =
+                        if w < item.weight {
+                            dp[i-1][w]
+                        } else {
+                            max(dp[i-1][w], dp[i-1][w-item.weight] + item.value )
+                        }
+                }
+        });
+        print_knapsack(&dp);
+
+        assert_eq!( dp[list.len()-1][capacity], 14 );
     }
 
     #[test]

bd1ea3728c26aa5c6cd3788fadc60ebbd44644cf
george velimachitis <3185416+gvelim@users.noreply.github.com>
2023-02-14 12:11:01 +0000

    DP Coins - non-recursive refactor - get_coins() returns the coin values used - get_coins_num()

--- src/dp/coin.rs
+++ src/dp/coin.rs
@@ -4,8 +4,8 @@ use std::time::SystemTime;
 
 #[test]
 fn test_sum_of_coins() {
-    let set = &[1,2,3,5,10,50];
-    let sum = 197;
+    let set = &[1,2,3,5];
+    let sum = 19;
     let mut coins = Coins::default();
 
     let mut start = SystemTime::now();
@@ -14,34 +14,57 @@ fn test_sum_of_coins() {
              SystemTime::now().duration_since(start)
     );
     start = SystemTime::now();
-    println!("Iterative: {:?}, {:?}",
-             Coins::iterative(sum, set),
+    let coins = Coins::iterative(sum, set);
+    println!("Iterative: {sum} = {:?}, {:?}",
+             (coins.get_coins_num(),coins.get_coins()),
              SystemTime::now().duration_since(start)
     );
 }
 
 struct Coins {
-    map: HashMap<usize,usize>
+    map: HashMap<usize,usize>,
+    dp: Option<Vec<usize>>,
+    coins: Option<Vec<usize>>
 }
 impl Default for Coins {
     fn default() -> Self {
-        Coins { map: HashMap::new() }
+        Coins { map: HashMap::new(), dp: None, coins: None }
     }
 }
 impl Coins {
-    fn iterative(sum: usize, coins:&[usize]) -> usize {
+    fn iterative(sum: usize, coins:&[usize]) -> Coins {
         let mut dp = vec![0;sum+1];
+        let mut cs= vec![0;sum+1];
         (1..dp.len())
             .for_each(|sum| {
-                dp[sum] = coins
+                (cs[sum], dp[sum]) = coins
                     .iter()
                     .filter(|&c| sum >= *c)
-                    .map(|&c| dp[sum - c] + 1 )
-                    .min()
-                    .unwrap()
+                    .map(|&c| (c,dp[sum - c] + 1) )
+                    .min_by_key(|t| t.1 )
+                    .unwrap();
             });
-        println!("{:?}",dp);
-        *dp.last().unwrap()
+        Coins {
+            map: HashMap::new(),
+            dp: Some(dp),
+            coins: Some(cs)
+        }
+    }
+    fn get_coins_num(&self) -> usize {
+        if let Some(dp) = &self.dp {
+            *dp.last().unwrap()
+        } else { 0 }
+    }
+    fn get_coins(&self) -> Vec<usize> {
+        let mut output = vec![];
+        if let Some(coins) = &self.coins {
+            let mut pos = coins.len() - 1;
+            while pos > 0 {
+                output.push(coins[pos]);
+                pos -= coins[pos];
+            }
+        }
+        output
     }
     fn recursive(&mut self, sum: usize, coins:&[usize]) -> usize {
         if sum == 0 {
